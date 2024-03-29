0c7be510a70ccdcbc535efd239251d3345bb075d
Ernesto Rodriguez <ernestorodriguez374@gmail.com>
2024-03-29 23:26:29 +0800

    Done Creation of Equipment

--- Creation_of_Equipment/src/main/java/trax/aero/application/Service.java
+++ Creation_of_Equipment/src/main/java/trax/aero/application/Service.java
@@ -1,17 +1,25 @@
 package trax.aero.application;
 
+import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 
 import trax.aero.controller.Creation_Equipment_Controller;
 import trax.aero.data.Creation_Equipment_Data;
 import trax.aero.logger.LogManager;
+import trax.aero.pojo.INT5_TRAX;
+import trax.aero.pojo.OpsLineEmail;
 
 @Path("/CreationEquipment")
 public class Service {
@@ -83,6 +91,58 @@ public class Service {
 		}
 		
 		return Response.ok(group, MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Path("/markTransaction")
+	@Consumes(MediaType.APPLICATION_XML + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_XML + ";charset=utf-8")
+	public Response markTransaction(INT5_TRAX input) {
+String executed = "OK";
+		
+		Creation_Equipment_Data data = new Creation_Equipment_Data("mark");
+		try {
+			JAXBContext jc = JAXBContext.newInstance(INT5_TRAX.class);
+			Marshaller marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			StringWriter sw = new StringWriter();
+			marshaller.marshal(input,  sw);
+			
+			String xmlContent = sw.toString();
+			
+			int xmlStart = xmlContent.indexOf("<?xml");
+			if(xmlStart > 0) {
+				xmlContent = xmlContent.substring(xmlStart);
+			}
+			
+			 xmlContent = xmlContent.replaceAll("[^\\x20-\\x7e]", "");
+		     
+			 logger.info("Input: " + xmlContent);
+			 
+			 executed = data.markTransaction(input);
+			 if(!executed.equalsIgnoreCase("OK")) {
+				 executed = "Issue Found";
+				 throw new Exception("Issue found");
+			 }
+			
+		}catch(Exception e) {
+			logger.severe(e.toString());
+			Creation_Equipment_Controller.addError(e.toString());
+				OpsLineEmail opsLineEmail = data.getOpsLineStaffName(input.getWO());
+				Creation_Equipment_Controller.sendEmailOpsLine(null, input, opsLineEmail);
+			
+		}finally {
+			try {
+				if(data.getCon() != null && !data.getCon().isClosed())
+					data.getCon().close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			logger.info("finishing");
+		}
+		
+		
+		return Response.ok(executed, MediaType.APPLICATION_XML + ";chartset=utf-8").build();
 	}
 
 }
